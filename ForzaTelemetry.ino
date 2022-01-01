d536cf71194f0598fa5b940d7d6fc21a08aa7a87
Matthew_Cash <matthew@matthew-cash.com>
2022-01-01 10:47:17 -0800

    Add 7seg display

--- ForzaTelemetry.ino
+++ ForzaTelemetry.ino
@@ -5,21 +5,34 @@
 #define LED_PIN_4 5
 #define LED_PIN_5 6
 
-void setup()
-{
-    Serial.begin(9600);
-    Serial.println("Serial Connected");
+#define SEG_PIN_TL 12
+#define SEG_PIN_T 13
+#define SEG_PIN_TR 7
+#define SEG_PIN_M 11
+#define SEG_PIN_BL 10
+#define SEG_PIN_B 9
+#define SEG_PIN_BR 8
 
-    pinMode(LED_PIN_1, OUTPUT);
-    pinMode(LED_PIN_2, OUTPUT);
-    pinMode(LED_PIN_3, OUTPUT);
-    pinMode(LED_PIN_4, OUTPUT);
-    pinMode(LED_PIN_5, OUTPUT);
-}
+char SEG_PINS[] = {SEG_PIN_TL, SEG_PIN_T, SEG_PIN_TR, SEG_PIN_M, SEG_PIN_BL, SEG_PIN_B, SEG_PIN_BR};
+
+char SEG_CHAR_R[] = {0, 0, 0, 1, 1, 0, 0};
+char SEG_CHAR_N[] = {0, 0, 0, 1, 1, 0, 1};
+char SEG_CHAR_0[] = {1, 1, 1, 0, 1, 1, 1};
+char SEG_CHAR_1[] = {0, 0, 1, 0, 0, 0, 1};
+char SEG_CHAR_2[] = {0, 1, 1, 1, 1, 1, 0};
+char SEG_CHAR_3[] = {0, 1, 1, 1, 0, 1, 1};
+char SEG_CHAR_4[] = {1, 0, 1, 1, 0, 0, 1};
+char SEG_CHAR_5[] = {1, 1, 0, 1, 0, 1, 1};
+char SEG_CHAR_6[] = {1, 1, 0, 1, 1, 1, 1};
+char SEG_CHAR_7[] = {0, 1, 1, 0, 0, 0, 1};
+char SEG_CHAR_8[] = {1, 1, 1, 1, 1, 1, 1};
+char SEG_CHAR_9[] = {1, 1, 1, 1, 0, 1, 1};
+
+char *SEG_NUMS[] = {SEG_CHAR_0, SEG_CHAR_1, SEG_CHAR_2, SEG_CHAR_3, SEG_CHAR_4, SEG_CHAR_5, SEG_CHAR_6, SEG_CHAR_7, SEG_CHAR_8, SEG_CHAR_9};
 
 unsigned int engine_max_rpm = 0;
 unsigned int engine_current_rpm = 0;
-unsigned char gear = 0;
+char gear = 0;
 
 inline char should_all_leds_flash()
 {
@@ -34,6 +47,49 @@ inline char should_led_light_up(unsigned int led_pin)
 unsigned int flash_counter = 0;
 #define FLASH_COUNTER_MAX 30000
 
+void update_gear()
+{
+    for (int i = 0; i < 7; i++)
+    {
+        digitalWrite(SEG_PINS[i], SEG_NUMS[gear][i]);
+    }
+}
+
+void update_rpms()
+{
+    if (!should_all_leds_flash())
+    {
+        digitalWrite(LED_PIN_1, should_led_light_up(1));
+        digitalWrite(LED_PIN_2, should_led_light_up(2));
+        digitalWrite(LED_PIN_3, should_led_light_up(3));
+        digitalWrite(LED_PIN_4, should_led_light_up(4));
+        digitalWrite(LED_PIN_5, should_led_light_up(5));
+    }
+}
+
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println("Serial Connected");
+
+    pinMode(LED_PIN_1, OUTPUT);
+    pinMode(LED_PIN_2, OUTPUT);
+    pinMode(LED_PIN_3, OUTPUT);
+    pinMode(LED_PIN_4, OUTPUT);
+    pinMode(LED_PIN_5, OUTPUT);
+
+    pinMode(SEG_PIN_TL, OUTPUT);
+    pinMode(SEG_PIN_T, OUTPUT);
+    pinMode(SEG_PIN_TR, OUTPUT);
+    pinMode(SEG_PIN_M, OUTPUT);
+    pinMode(SEG_PIN_BL, OUTPUT);
+    pinMode(SEG_PIN_B, OUTPUT);
+    pinMode(SEG_PIN_BR, OUTPUT);
+
+    update_gear();
+    update_rpms();
+}
+
 void loop()
 {
     if (Serial.available() >= 7)
@@ -41,9 +97,26 @@ void loop()
         // Game data starts with two 0xff bytes
         if (Serial.read() == 0xff && Serial.read() == 0xff)
         {
-            engine_max_rpm = Serial.read() << 8 + Serial.read();
-            engine_current_rpm = Serial.read() << 8 + Serial.read();
-            gear = Serial.read();
+            unsigned int new_engine_max_rpm = Serial.read() << 8 + Serial.read();
+            if (new_engine_max_rpm != engine_max_rpm)
+            {
+                engine_max_rpm = new_engine_max_rpm;
+                update_rpms();
+            }
+
+            unsigned int new_engine_current_rpm = Serial.read() << 8 + Serial.read();
+            if (new_engine_current_rpm != engine_current_rpm)
+            {
+                engine_current_rpm = new_engine_current_rpm;
+                update_rpms();
+            }
+
+            unsigned char new_gear = Serial.read();
+            if (new_gear != gear)
+            {
+                gear = Serial.read();
+                update_gear();
+            }
         }
     }
 
@@ -61,13 +134,5 @@ void loop()
             digitalWrite(LED_PIN_4, !digitalRead(LED_PIN_4));
             digitalWrite(LED_PIN_5, !digitalRead(LED_PIN_5));
         }
-    }
-    else
-    {
-        digitalWrite(LED_PIN_1, should_led_light_up(1));
-        digitalWrite(LED_PIN_2, should_led_light_up(2));
-        digitalWrite(LED_PIN_3, should_led_light_up(3));
-        digitalWrite(LED_PIN_4, should_led_light_up(4));
-        digitalWrite(LED_PIN_5, should_led_light_up(5));
     }
 }
